680e44af39b0f5b140955f7837a0679688b06d83
Jatin Arutla <50556574+JatinArutla@users.noreply.github.com>
2021-06-30 14:18:42 +0530

    Update simple.py

--- apps/simple.py
+++ apps/simple.py
@@ -5,10 +5,7 @@ import pandas as pd
 def app():
     import streamlit as st
     import pandas as pd
-    import numpy as np
-    # import nltk
-    # from nltk.stem import WordNetLemmatizer
-    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.feature_extraction.text import TfidfVectorizer
     from sklearn.metrics.pairwise import cosine_similarity
 
     header = st.beta_container()
@@ -27,40 +24,17 @@ def app():
 
     with pre_processing:
 
-        netflix_shows[['duration', 'season']] = netflix_shows['duration'].str.split(' ', 1, expand=True)
-        netflix_shows.drop(columns=['season'], axis=1, inplace=True)
-        netflix_shows['rating'] = netflix_shows['rating'].replace('TV-MA', 'R')
-        netflix_shows['rating'] = netflix_shows['rating'].replace('TV-14', 'TV-PG')
-        netflix_shows['rating'] = netflix_shows['rating'].replace('TV-G', 'TV-PG')
+        df = pd.read_csv('preprocessed.csv')
 
-        def cleaning_data(a):
-            return str.lower(a.replace(",", " "))
-        def no_space(x):
-            return str.lower(x.replace(" ", ""))
-
-        df = netflix_shows.fillna('')
-        df.head(2)
-        df['release_year'] = df['release_year'].apply(str)
-        features = ['title', 'description', 'rating']
-        df = df[features]
-        for feature in features:
+        features1 = ['title', 'cast', 'description', 'genre', 'duration', 'release_year', 'country', 'rating']
+        df = df[features1]
+        for feature in features1:
             df[feature] = df[feature].apply(str)
-        for feature in features:
-            df[feature] = df[feature].apply(cleaning_data)
-        df['title'] = df['title'].apply(no_space)
-
-        
-        # def get_lemmatized_text(corpus):
-        #     lemmatizer = WordNetLemmatizer()
-        #     return [' '.join([lemmatizer.lemmatize(word) for word in review.split()]) for review in corpus]
-
-        # df['description'] = get_lemmatized_text(df['description'])
 
         def create_soup(a):
             return a['title'] + ' ' + a['description'] + ' ' + a['rating']
         df['soup'] = df.apply(create_soup, axis=1)
-
-        count = CountVectorizer(stop_words='english')
+        count = TfidfVectorizer(stop_words='english')
         count_matrix = count.fit_transform(df['soup'])
         cosine_sim = cosine_similarity(count_matrix, count_matrix)
         df.reset_index(drop=True, inplace=True)
@@ -78,6 +52,6 @@ def app():
             df.index = range(1,len(df)+1)
             return df
 
-        result = recommendation(options, cosine_sim)
+        result= recommendation(options, cosine_sim)
         st.write('Here are the recommended TV shows which are available on Netflix and are most similar to your selection.')
         st.write(result)
